b9c6e205673ea392443ea8af120ce15d18a15a54
Jack Shone <rc0028232@student.riversidecollege.ac.uk>
2017-09-13 10:01:14 +0100

    Display Hello World on button press.

--- Hello World/Form1.Designer.cs
+++ Hello World/Form1.Designer.cs
@@ -37,14 +37,15 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 30);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Hello";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 468);
+            this.ClientSize = new System.Drawing.Size(532, 370);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
